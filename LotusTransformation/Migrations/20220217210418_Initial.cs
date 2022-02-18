544299e431937bd3ecf23fb7d2f5a634a6249dbc
ABWeb-Dev <abradford.webdev@gmail.com>
2022-02-18 14:43:40 -0800

    Trying out Relationships and establishing them between existing tables

---
+++ LotusTransformation/Migrations/20220217210418_Initial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LotusTransformation.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInformation",
+                columns: table => new
+                {
+                    UserID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MiddleInitial = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StateOrProvince = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZIPorPostal = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryPhoneType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondaryEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SecondaryPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondaryPhoneType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInformation", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LogIn",
+                columns: table => new
+                {
+                    UserID = table.Column<long>(type: "bigint", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogIn", x => x.UserID);
+                    table.ForeignKey(
+                        name: "FK_LogIn_UserInformation_UserID",
+                        column: x => x.UserID,
+                        principalTable: "UserInformation",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogIn");
+
+            migrationBuilder.DropTable(
+                name: "UserInformation");
+        }
+    }
+}
